f0651ef0c0137c8c9d57714ec402723c61da9395
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-05-04 16:09:41 +0200

    Added simple banner

--- src/layout/HeroSection/HeroSection.svelte
+++ src/layout/HeroSection/HeroSection.svelte
@@ -2,4 +2,5 @@
     import { Button } from "fluent-svelte";
 </script>
 
+<img src="https://www.withanaccent.com/wp-content/uploads/2019/01/john-wick-3-banner.jpg">
 <Button variant={"accent"}>Test button</Button>
