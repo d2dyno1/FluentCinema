3a1d5afd06d469ab18ef1ff0c4d66ccd7214f7e5
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-05 21:40:24 +0200

    Added an endpoint for deleting accounts

---
+++ src/routes/api/deleteaccount.ts
@@ -0,0 +1,29 @@
+import {client, getUser} from '$lib/database/db';
+import {badRequest, forbidden, internalServerError, ok} from "../../lib/responses";
+import { verify } from "$lib/argon2";
+
+export async function del({ request }) {
+    try {
+        const data = await request.json();
+
+        if (!data.hasOwnProperty("email") || !data.hasOwnProperty("password")) {
+            return badRequest("Missing credentials.");
+        }
+
+        let user = await getUser(data.email);
+        if (user == undefined) {
+            return badRequest("This e-mail address is not associated with any account.");
+        }
+
+        if (!await verify(user.hashed_password, data.password)) {
+            return forbidden("Incorrect password.");
+        }
+
+        await client.query("DELETE FROM users WHERE id=$1", [user.id]);
+
+        return ok;
+    } catch (e) {
+        console.log(e);
+        return internalServerError;
+    }
+}
