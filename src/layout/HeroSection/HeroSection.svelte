f51d02a9cecac7b0a5120cd408a06f224b030c22
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-05-05 21:43:21 +0200

    Added fade to banner

--- src/layout/HeroSection/HeroSection.svelte
+++ src/layout/HeroSection/HeroSection.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { Button, Tooltip } from "fluent-svelte";
+    import { fade } from "svelte/transition";
+    import { Button } from "fluent-svelte";
     import { MovieCard } from "$lib";
     import type { MovieData } from "$data/movies";
 
@@ -15,13 +16,14 @@
 <div class="main-content">
     <div class="hero-section">
         <picture>
-            <img class="banner-img" alt={currentCard.name} src={currentCard.image}>
+            <img transition:fade src={currentCard.image} alt={currentCard.name} class="banner-img">
         </picture>
         <div class="hero-left">
             <h1>{currentCard.name}</h1>
             <p class="banner-subtitle">{currentCard.description}</p>
             <Button class="details-button">See details</Button>
         </div>
+        <div class="hero-overlay"></div>
     </div>
     <div class="cards">
         {#each cards as card}
