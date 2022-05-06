a8777db474303933af480cfd41feb195e32faba4
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-06 12:44:18 +0200

    Initial work on settings

---
+++ src/layout/ActionBlock/ActionBlock.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import {TextBlock} from "fluent-svelte";
+
+    export let title: string;
+    export let description: string;
+    export let showArrow = true;
+</script>
+
+<div class="category">
+    <div class="icon">
+        <slot name="icon"/>
+    </div>
+    <div class="column">
+        <TextBlock variant="bodyStrong">{title}</TextBlock>
+        <TextBlock>{description}</TextBlock>
+    </div>
+    <div class="align-right">
+        <slot name="action"/>
+        {#if showArrow}
+            <TextBlock>＞</TextBlock>
+        {/if}
+    </div>
+</div>
+
+<style lang="scss">
+    @use "ActionBlock";
+</style>
--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,3 +1,4 @@
+export { default as ActionBlock } from "./ActionBlock/ActionBlock.svelte";
 export { default as DialogForm } from "./DialogForm/DialogForm.svelte";
 export { default as HeroSection } from "./HeroSection/HeroSection.svelte";
 export { default as LoginFlyout } from "./LoginFlyout/LoginFlyout.svelte"
