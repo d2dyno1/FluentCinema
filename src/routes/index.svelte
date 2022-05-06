e5ee02772297a84737b641719ef2e64f2ef05870
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-06 21:38:09 +0200

    Improved code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,9 @@
 <script lang="ts" context="module">
     import { HeroSection, MoviesSection } from "$layout";
-    import type { MovieData } from "$data/movies";
     import { LoginFlyout } from "$layout";
     import { ok } from "../lib/responses";
 
     export let promise;
-    export let headlineMovies: MovieData[];
-    export let allMovies: MovieData[];
 
     export async function load({ fetch }) {
         promise = fetch("/api/movies", {
@@ -14,20 +11,15 @@
                 'Content-Type': 'application/json'
             },
             method: 'GET'
-        }).then(response => response.json()).then(movies => {
-            headlineMovies = movies;
-            allMovies = [...movies];
-        });
+        }).then(response => response.json())
 
         return ok;
     }
 </script>
 
-{#await promise}
-    <p>Loading movies...</p>
-{:then value}
-    <HeroSection cards={headlineMovies}/>
-    <MoviesSection positions={allMovies}/>
+{#await promise then movies}
+    <HeroSection cards={movies}/>
+    <MoviesSection positions={movies}/>
 {/await}
 
 <LoginFlyout/>
