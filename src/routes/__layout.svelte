a623bca0fdca3cf1ec71312fe5cbc656398085de
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-05-06 12:31:29 +0200

    Added Navbar items

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
     import { Navbar } from "$layout";
-    import "fluent-svelte/theme.css";
+    import type { NavbarButton, NavbarItem } from "src/data/navbar";
+
+    const navbarItems: NavbarItem[] = [
+        {
+            name: "Home",
+            path: "/",
+            icon: null // TODO: Home icon (fluentui icons)
+        }
+    ];
+    const navbarButtons: NavbarButton[] = [
+        {
+            name: "test",
+            path: "/",
+            icon: "ABCD"
+        }
+    ]
 </script>
 
-<Navbar/>
+<Navbar navbarItems={navbarItems} navbarButtons={navbarButtons}/>
 <slot/>
 <!-- <Footer/> -->
 
 <style global lang="scss">
-  @use "src/styles/global";
+    @use "src/styles/global";
 
-  /* Needed for adaptive theme */
-  :global(body) {
-    margin: 0px;
-    background-color: var(--fds-solid-background-base);
-    color: var(--fds-text-primary);
-  }
+    /* Needed for adaptive theme */
+    :global(body) {
+        background-color: var(--fds-solid-background-base);
+        color: var(--fds-text-primary);
+        margin: 0px;
+    }
 </style>
