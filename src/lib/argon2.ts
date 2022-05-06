eae9fed8b8d7432c6869861d2bc5fced109836b7
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-06 08:59:21 +0200

    Added password hashing with argon2

--- src/lib/argon2.ts
+++ src/lib/argon2.ts
@@ -1,15 +1,13 @@
 import * as argon2 from "argon2";
 
-// const argon2Options = {
-//     type: argon2.argon2id
-// };
+const argon2Options = {
+    type: argon2.argon2id
+};
 
 export async function hash(password: string) {
-    return password;
-    // return await argon2.hash(password, argon2Options)
+    return await argon2.hash(password, argon2Options)
 }
 
 export async function verify(hash: string, password: string) {
-    return password == hash;
-    // return await argon2.verify(hash, password, argon2Options)
+    return await argon2.verify(hash, password, argon2Options)
 }
