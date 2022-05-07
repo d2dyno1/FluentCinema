70d479bcf1cbb76adae1b65f647247fee0ae0763
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-07 10:46:33 +0200

    Initial setup for i18n

---
+++ src/lib/i18n/i18n.ts
@@ -0,0 +1,13 @@
+import { addMessages, init } from "svelte-i18n";
+
+import en_US from "./locales/en_US.json";
+import pl_PL from "./locales/pl_PL.json";
+
+// @ts-ignore
+addMessages("en_US", en_US);
+// @ts-ignore
+addMessages("pl_PL", pl_PL);
+
+init({
+    fallbackLocale: "en_US"
+});
