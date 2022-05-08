7cf1597991992f8bb4800927517f082c9769b51d
d2dyno <53011783+d2dyno1@users.noreply.github.com>
2022-05-08 12:56:45 +0200

    Improved MoviesSection (#19)

--- src/layout/MoviesSection/MoviesSection.svelte
+++ src/layout/MoviesSection/MoviesSection.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+    import { Button } from "fluent-svelte";
     import { MoviePosition } from "$lib";
     import type { MovieData } from "$data/movies";
 
     export let positions: MovieData[] = [];
 </script>
 
-<div class="movie-positions">
-    {#each positions as { name, description, descriptionExtended, rating, posterImage }}
+<div class="wrapper">
+    <div class="header">
+        <h2>
+            Explore
+        </h2>
+        <Button variant="hyperlink" on:click>See all</Button> <!-- TODO: Add click -->
+    </div>
+    <div class="movie-positions">
+        {#each positions as { name, description, descriptionExtended, rating, posterImage }}
         <MoviePosition name={name} episode={description} description={descriptionExtended} rating={rating} image={posterImage} />
-    {/each}
+        {/each}
+    </div>
 </div>
 
 <style lang="scss">
