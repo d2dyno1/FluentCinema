2897452c2c5a3cbeaf678e64412ff59124f3af59
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-09 18:10:20 +0200

    Improved API validation

--- src/routes/api/account/email/verify.ts
+++ src/routes/api/account/email/verify.ts
@@ -2,20 +2,26 @@ import {badRequest, internalServerError, missingParameters, ok} from "../../../.
 import {isTokenValid, verifyEmail} from "../../../../lib/auth/emailverification";
 import type { RequestHandler } from "@sveltejs/kit";
 import type {VerifyEmailParams} from "../../../../data/params/VerifyEmailParams";
+import {object, string} from "yup";
+import type {InferType} from "yup";
+import {emailValidationRegex} from "../../../../lib/validation";
 
+const verifyEmailSchema = object({
+    token: string().required()
+});
+interface VerifyEmailSchema extends InferType<typeof verifyEmailSchema> {}
 
 export const post: RequestHandler = async ({ request }) => {
     try {
-        let params: VerifyEmailParams = await request.json();
-        if (params.token == null) {
-            return missingParameters;
-        }
-
-        let token = params.token;
-        if (!await isTokenValid(token)) {
+        let params: VerifyEmailSchema = await request.json();
+        if (!await verifyEmailSchema.isValid(params)) {
             return badRequest;
         }
-        await verifyEmail(token);
+
+        if (!await isTokenValid(params.token)) {
+            return badRequest;
+        }
+        await verifyEmail(params.token);
 
         return ok;
     } catch (e) {
--- src/routes/api/account/login.ts
+++ src/routes/api/account/login.ts
@@ -1,5 +1,6 @@
 import { getUser } from '../../../lib/db';
 import {
+    badRequest,
     badRequestWithMessage,
     forbidden,
     forbiddenWithMessage,
@@ -11,39 +12,40 @@ var exports = {}; // dirty hack to make cookie work
 import {parse, serialize} from "cookie";
 import {generateSessionCookie, getSessionFromRequest, isSessionValid} from "../../../lib/auth/sessions";
 import type { RequestHandler } from "@sveltejs/kit";
-import type { LoginParams } from "../../../data/params/LoginParams";
+import {object, string} from "yup";
+import type {InferType} from "yup";
+import {emailValidationRegex, passwordValidationRegex,} from "../../../lib/validation";
+
+const loginSchema = object({
+    email: string().required().matches(emailValidationRegex),
+    password: string().required().matches(passwordValidationRegex)
+});
+interface LoginSchema extends InferType<typeof loginSchema> {}
 
 export const post: RequestHandler = async ({ request }) => {
-    try {
-        if (await isSessionValid(getSessionFromRequest(request))) {
-            return forbidden;
-        }
-
-        let params: LoginParams = await request.json();
-        if (params.email == null || params.password == null) {
-            return missingParameters;
-        }
-
-        let user = await getUser(params.email);
-        if (user == null) {
-            return badRequestWithMessage("Account does not exist.");
-        }
-
-        if (!await verify(user.hashed_password, params.password)) {
-            return forbiddenWithMessage("Incorrect password.");
-        }
-
-        return {
-            status: 200,
-            body: {
-                success: true
-            },
-            headers: {
-                'Set-Cookie': await generateSessionCookie(user)
-            }
-        };
-    } catch (e) {
-        console.log(e);
-        return internalServerError;
+    if (await isSessionValid(getSessionFromRequest(request))) {
+        return forbidden;
     }
+
+    let params: LoginSchema = await request.json();
+    if (!await loginSchema.validate(params)) {
+        return badRequest;
+    }
+
+    let user = await getUser(params.email);
+    if (user == null) {
+        return badRequestWithMessage("Account does not exist.");
+    } else if (!await verify(user.hashed_password, params.password)) {
+        return forbiddenWithMessage("Incorrect password.");
+    }
+
+    return {
+        status: 200,
+        body: {
+            success: true
+        },
+        headers: {
+            'Set-Cookie': await generateSessionCookie(user)
+        }
+    };
 }
