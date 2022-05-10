76512852d9683af72aab0f9dc409b71dcac78fce
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-10 20:10:44 +0200

    Added user settings

---
+++ src/routes/api/account/settings/update.ts
@@ -0,0 +1,22 @@
+import {badRequest, ok, forbidden} from "../../../../lib/responses";
+import type { RequestHandler } from "@sveltejs/kit";
+import {Session} from "../../../../lib/db/Session";
+import {settingsSchema} from "../../../../data/schema/SettingsSchema";
+import type {SettingsSchema} from "../../../../data/schema/SettingsSchema";
+
+export const put: RequestHandler = async ({ request }) => {
+    let session = await Session.getFromRequest(request);
+    if (session == null) {
+        return forbidden;
+    }
+
+    let params: SettingsSchema = await request.json();
+    if (!await settingsSchema.isValid(params)) {
+        return badRequest;
+    }
+
+    let user = await session.getUser();
+    let settings = await user.getSettings();
+    await settings.update(params);
+    return ok;
+}
