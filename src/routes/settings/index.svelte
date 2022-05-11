5af27d45f9a047def54c8861f81b2a82e6037d06
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-11 21:05:20 +0200

    Added a language setting

--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -1,9 +1,23 @@
 <script lang="ts" context="module">
     import { ActionBlock } from "$lib";
-    import { Button } from "fluent-svelte";
+    import { Button, ComboBox } from "fluent-svelte";
     import {ok} from "../../lib/responses";
+    import type {Session} from "../../data/Session";
 
+    import LanguageIcon from "@fluentui/svg-icons/icons/local_language_24_filled.svg?raw";
 
+    const languages = [
+        {
+            name: "English",
+            value: "en_US"
+        },
+        {
+            name: "Polish",
+            value: "pl_PL"
+        }
+    ];
+
+    let _session: Session;
     let files: HTMLInputElement;
     function uploadPicture() {
         fetch("/api/account/picture/upload", {
@@ -12,7 +26,15 @@
         }).then(() => window.location.reload());
     }
 
+    function uploadSettings() {
+        fetch("/api/account/settings/update", {
+            method: "PUT",
+            body: JSON.stringify(_session.user.settings)
+        });
+    }
+
     export async function load({ session }) {
+        _session = session;
         if (!session.isLoggedIn) {
             return {
                 status: 302,
@@ -28,13 +50,10 @@
     <Button on:click={uploadPicture}>upload</Button>
 </div>
 
-<ActionBlock title="Password" description="a">
-    <Button slot="action">Change</Button>
+<ActionBlock title="Language" description="placeholder" icon={LanguageIcon}>
+    <ComboBox slot="action" items={languages} bind:value={_session.user.settings.language}></ComboBox>
 </ActionBlock>
-
-<ActionBlock isClickableEffect={true} title="Example category" description="Example description">
-    <svg slot="icon" style="filter: invert(100%)" width="32" height="32" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M8 7c.28 0 .5.22.5.5v3a.5.5 0 01-1 0v-3c0-.28.22-.5.5-.5z"></path><path d="M8 6.25a.75.75 0 100-1.5.75.75 0 000 1.5z"></path><path d="M2 8a6 6 0 1112 0A6 6 0 012 8zm6-5a5 5 0 100 10A5 5 0 008 3z"></path></svg>
-</ActionBlock>
+<Button on:click={uploadSettings}>Save settings</Button>
 
 <style lang="scss">
     @use "settings";
