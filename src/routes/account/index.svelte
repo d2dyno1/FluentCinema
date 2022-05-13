88e34aadd92e4125dbd32f846ed78f9630f58a40
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-05-13 22:21:59 +0200

    Fixed navigation

--- src/routes/account/index.svelte
+++ src/routes/account/index.svelte
@@ -3,6 +3,16 @@
     import { AccountCard } from "$lib";
     import { ok } from "$lib/responses";
     import type { Session } from "$data/Session";
+
+    let session_: Session;
+
+    export async function load({ session }) {
+        session_ = session;
+        return ok;
+    }
+</script>
+
+<script lang="ts">
     import Preferences from "./preferences/preferences.svelte";
     import Reservations from "./reservations/reservations.svelte";
 
@@ -16,17 +26,10 @@
             component: Reservations
         }
     ];
-
-    let session_: Session;
-    let selectedItem: NavigationItem = navItems[0];
-
-    export async function load({ session }) {
-        session_ = session;
-        return ok;
-    }
+    let selectedItem = navItems[0];
 </script>
 
-<div>
+<div class="wrapper">
     <AccountCard bind:selectedItem={selectedItem} session={session_} navItems={navItems}/>
     <svelte:component this={selectedItem.component}/>
 </div>
