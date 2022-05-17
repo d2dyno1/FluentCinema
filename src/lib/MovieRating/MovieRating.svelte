c86f07ac7df378c50927cc971241820b66d1ce4b
taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-17 10:16:21 +0000

    Fixed movie rating

--- src/lib/MovieRating/MovieRating.svelte
+++ src/lib/MovieRating/MovieRating.svelte
@@ -2,21 +2,27 @@
     import StarRating from "svelte-star-rating";
     import { TextBlock } from "fluent-svelte";
     import type { Movie } from "$db/movie/Movie";
+    import {onMount} from "svelte";
     
     export let movie: Movie;
 
-    const computedTheme = getComputedStyle(document.body);
-    const config = {
-        fullColor: computedTheme.getPropertyValue('--fds-text-secondary'),
-        emptyColor: "hsla(0, 0%, 0%, 12%)",
-        size: 16
-    }
+    let config;
 
+    onMount(() => {
+        let computedTheme = getComputedStyle(document.body);
+        config = {
+            fullColor: computedTheme.getPropertyValue('--fds-text-secondary'),
+            emptyColor: "hsla(0, 0%, 0%, 12%)",
+            size: 16
+        }
+    });
 </script>
 
 <div class="movie-rating">
-    <StarRating {config} rating={movie.rating}/>
-    <TextBlock variant="caption">{movie.rating == 0 ? "No reviews" : `${movie.rating}/5`}</TextBlock>
+    {#if config != null}
+        <StarRating {config} rating={movie.rating}/>
+        <TextBlock variant="caption">{movie.rating == 0 ? "No reviews" : `${movie.rating}/5`}</TextBlock>
+    {/if}
 </div>
 
 <style lang="scss">
