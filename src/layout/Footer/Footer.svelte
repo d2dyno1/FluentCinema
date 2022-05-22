3b124ab0a5004067070bee724207ac1eb72b826b
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-05-22 20:52:51 +0200

    Added footer

--- src/layout/Footer/Footer.svelte
+++ src/layout/Footer/Footer.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
+    import { Button } from "fluent-svelte";
+
     
 </script>
 
 <div class="footer-wrapper">
-    <div class="site-name-column">
+    <div class="column title-column">
         <h2>
             FluentCinema
         </h2>
+        <div class="column-title copyright">
+            Copyright © 2022
+        </div>
+    </div>
+    <div class="site-details">
+        <div class="column">
+            <div class="column-title">
+                Links
+            </div>
+            <Button variant="hyperlink" href="/">Home</Button>
+        </div>
+        <div class="column">
+            <div class="column-title">
+                Team
+            </div>
+            <Button variant="hyperlink" href="https://github.com/taj-ny" target="_blank">Taj_ny</Button>
+            <Button variant="hyperlink" href="https://github.com/d2dyno1" target="_blank">d2dyno</Button>
+        </div>
     </div>
 </div>
 
