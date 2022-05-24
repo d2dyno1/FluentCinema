04938b3a1d96c5bf47955a84a90a7285f2d8d340
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-05-24 17:40:06 +0200

    Fixed movie types and added screening types

--- src/api/ScreeningApiContext.ts
+++ src/api/ScreeningApiContext.ts
@@ -2,6 +2,7 @@ import type { IScreening } from "$data/model/IScreening";
 import type { MovieApiContext } from "./MovieApiContext";
 import type { ScreeningRoomApiContext } from "./ScreeningRoomApiContext";
 import type { Fetch } from "./ApiContext";
+import type { ScreeningType } from "$data/ScreeningType";
 
 export class ScreeningApiContext implements IScreening {
     readonly movie: MovieApiContext;
@@ -10,6 +11,7 @@ export class ScreeningApiContext implements IScreening {
     readonly soldOut: boolean;
     readonly start: Date;
     readonly seat: number;
+    readonly type: ScreeningType;
 
     constructor(screening: IScreening, movie?: MovieApiContext, room?: ScreeningRoomApiContext);
     constructor(screening: IScreening, movie: undefined, room: undefined) {
@@ -19,6 +21,7 @@ export class ScreeningApiContext implements IScreening {
         this.soldOut = screening.soldOut;
         this.start = new Date(screening.start);
         this.seat = screening.seat;
+        this.type = screening.type;
     }
 
     static async getFromMovieId(fetch: Fetch, movieId: string | number): Promise<ScreeningApiContext[]> {
---
+++ src/data/ScreeningType.ts
@@ -0,0 +1,6 @@
+export enum ScreeningType {
+    SUBTITLES_2D = "sub_2d",
+    SUBTITLES_3D = "sub_3d",
+    DUBBING_2D = "dub_2d",
+    DUBBING_3D = "dub_3d"
+}
