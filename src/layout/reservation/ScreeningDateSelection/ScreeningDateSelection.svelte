e50df4305053be8c6f76f57bdf2652ae2b09c50f
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-06-01 17:17:15 +0200

    Laid ground for date selection

--- src/layout/reservation/ScreeningDateSelection/ScreeningDateSelection.svelte
+++ src/layout/reservation/ScreeningDateSelection/ScreeningDateSelection.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
-    import { CalendarView } from "fluent-svelte";
+    import type { Movie } from "$api/Movie";
+    import type { TableDateItem } from "$data/table";
+    import { Button, CalendarView } from "fluent-svelte";
+    import moment from "moment";
 
+    export let movie: Movie;
+    export let screeningDates: TableDateItem[];
+    export let selectedDate: Date = new Date();
 
+    const yesterday = new Date();
+    const nextMonth = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    nextMonth.setMonth(nextMonth.getMonth() + 1);
 </script>
 
 <div class="wrapper">
-    <CalendarView/>
+    <CalendarView min={yesterday} max={nextMonth} bind:value={selectedDate}/>
+    <div class="date-section">
+        <div class="day-title">
+            {#if selectedDate}
+                {moment(selectedDate).format('dddd')}
+            {:else}
+                Select day...
+            {/if}
+        </div>
+        <div class="details">
+            {#if selectedDate}
+            <div class="time-list">
+                <Button variant="standard">17:30</Button>
+                <Button variant="accent">19:30</Button>
+                <Button variant="standard">22:00</Button>
+            </div>
+            <div class="screening-details">
+                
+            </div>
+            {/if}
+        </div>
+    </div>
 </div>
 
 <style lang="scss">
