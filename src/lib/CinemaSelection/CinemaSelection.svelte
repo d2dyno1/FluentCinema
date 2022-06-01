435fe41789632b8f194abad1e5acbf2b8b9d4832
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-06-01 11:57:54 +0200

    Fixed cinema selection map

--- src/lib/CinemaSelection/CinemaSelection.svelte
+++ src/lib/CinemaSelection/CinemaSelection.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-    import { onMount } from "svelte";
     import { Cinema } from "$api/Cinema";
     import LocationIcon from "@fluentui/svg-icons/icons/location_48_filled.svg?raw";
     import { TextBlock } from "fluent-svelte";
+    import { onMount } from "svelte";
 
     export let cinemas: Cinema[];
     export let selectedCinema: Cinema;
 
     let mapContainer;
+    let ready;
 
-    function initMap(): void {
+    function initMap() {
         let map = new google.maps.Map(mapContainer, {
             center: { lat: cinemas[0].latitude, lng: cinemas[0].longitude },
             zoom: 8
@@ -21,11 +22,15 @@
         }
     }
 
-    onMount(async () => {
+    onMount(() => {
         window.initMap = initMap;
-        eval(await (await fetch("https://maps.googleapis.com/maps/api/js?key=AIzaSyBunHici73nuGFjNgAB8cRoNvqK1nOGIxI&callback=initMap&v=weekly")).text());
-    })
+        ready = true;
+    });
 </script>
+
+{#if ready}
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBunHici73nuGFjNgAB8cRoNvqK1nOGIxI&callback=initMap&v=weekly"/>
+{/if}
 
 <div bind:this={mapContainer} id="map"></div>
 {#if selectedCinema != null}
